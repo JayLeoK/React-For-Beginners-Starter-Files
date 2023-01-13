e0dc39a82e02e5b6c9b3f566f3eea3481b3ecf11
Jaydon Krooss <jkrooss@gmail.com>
2023-01-13 17:07:35 -0500

    1-13 use helper fn

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import { render } from "react-dom";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
   render() {
@@ -11,7 +12,12 @@ class StorePicker extends React.Component {
         <form className="store-selector">
           <h2>Please enter a store</h2>
 
-          <input type="text" required placeholder="Store Name" />
+          <input
+            type="text"
+            required
+            placeholder="Store Name"
+            defaultValue={getFunName()}
+          />
           <button type="submit">Visit Store:</button>
         </form>
       </Fragment>
