a0d398025453b325aee97789af2b77d71ba022dd
Jaydon Krooss <jkrooss@gmail.com>
2023-01-14 17:18:30 -0500

    1-14: events, refs, binding, fish form, invventory state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,16 +2,45 @@ import React from "react";
 import Header from "./Header.js";
 import Inventory from "./Inventory.js";
 import Order from "./Order.js";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {},
+  };
+
+  addFish = (fish) => {
+    console.log("Adding a fish!");
+    //to update state:
+    //1. take copy of existing state (don't modify directly = mutation)
+    const fishes = { ...this.state.fishes }; //spreading!
+    //2. add new fish to the copy
+    fishes[`fish${Date.now()}`] = fish;
+    this.setState({ fishes });
+  };
+
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood is cool!" cool={true} />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map((key) => (
+              <Fish key={key} details={this.state.fishes[key]} />
+            ))}
+          </ul>
         </div>
         <Order />
-        <Inventory />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,17 +3,32 @@ import { render } from "react-dom";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  //property that defines the ref on the page
+  myInput = React.createRef();
+  //property that is set to arrow function
+  goToStore = (event) => {
+    //stop the form from submitting
+    event.preventDefault();
+
+    //get text from input
+    const storeName = this.myInput.current.value;
+    console.log(storeName);
+
+    //change page to /store/whatever
+    //use react router to keep up the memory of the pages
+    this.props.history.push(`/store/${storeName}`);
+  };
   render() {
     return (
       <Fragment>
         {/* only JS comments inserted when using JSX */}
         {/* also note the return is only one element (can be <> or <Fragment>)*/}
         <p>FISH!</p>
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please enter a store</h2>
-
           <input
             type="text"
+            ref={this.myInput}
             required
             placeholder="Store Name"
             defaultValue={getFunName()}
